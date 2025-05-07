819771a6d9335f6432f8e734f2e5380d66b54fd0
Chandan Gepala <c.gepala@cyens.org.cy>
2025-05-07 18:17:20 +0300

    Initial commit

---
+++ app/src/main/java/com/cyens/retrofituploadfile/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cyens.retrofituploadfile;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnUpload;
+    ImageView imgUpload;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initUI();
+        btnUpload.setOnClickListener(v -> {
+
+        });
+    }
+
+    void initUI(){
+        btnUpload = findViewById(R.id.btn_upload);
+        imgUpload = findViewById(R.id.img_upload);
+    }
+}
